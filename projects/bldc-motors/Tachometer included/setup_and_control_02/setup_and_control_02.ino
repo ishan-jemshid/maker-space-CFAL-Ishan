f1d660f2f35fbc9fea6533a2c1c368be5104fca5
ishan-jemshid <ishan.ubuntu@gmail.com>
2026-08-20 19:40:02 +0530

    made the speed change smoother

---
+++ projects/bldc-motors/Tachometer included/setup_and_control_02/setup_and_control_02.ino
@@ -0,0 +1,63 @@
+#include <Servo.h>
+
+Servo myESC;
+
+int speed = 0;
+int step = 0;
+
+void setup() {
+  Serial.begin(9600);
+  myESC.attach(9);
+
+  Serial.println("ESC_CALLIBERATION");
+  Serial.println("5. Press Enter in Serial Monitor when if the ESC battery is unplugged");
+  
+  while (Serial.available() == 0) {} // Wait for user to press Enter
+  while (Serial.available() > 0) { Serial.read(); } // Clear buffer
+  
+  Serial.println("Sending-Max-Throttle");
+  myESC.writeMicroseconds(2050);  //2050 instead of 2000 since during testing closest I could get it to max out was at 2050
+  
+  Serial.println("3. Plug in the ESC battery");
+  Serial.println("5. Press Enter in Serial Monitor after the beeps.");
+
+  while (Serial.available() == 0) {} // Wait for user to press Enter
+  while (Serial.available() > 0) { Serial.read(); } // Clear buffer
+
+  Serial.println("Sending MIN throttle (1000 us)...");
+//  delay(5000);
+  myESC.writeMicroseconds(942); //942 instead of 1000 since during testing I could get the min to be at exactly 942
+  
+  Serial.println("Calibration Complete");
+}
+
+void loop() {
+  
+  Serial.print("Speed : ");
+  while (Serial.available() == 0) {}
+
+  int temp_speed = Serial.parseInt();
+
+  while (Serial.available() > 0) {
+    Serial.read(); 
+
+  }
+  
+    Serial.println(temp_speed);
+
+    if (temp_speed > speed){
+      step = 1;
+    }
+    else{
+      step = -1;
+    }
+
+  while (temp_speed != round(speed)) {
+    speed = speed + step;
+    myESC.writeMicroseconds(speed);
+    delay(3);
+  }
+  myESC.writeMicroseconds(temp_speed);
+  speed = temp_speed;
+  
+}
